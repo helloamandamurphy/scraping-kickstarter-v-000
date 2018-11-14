ab93f267509e1b76174390075d062300ad5b5d21
Amanda Murphy <helloamandamurphy@gmail.com>
2018-11-14 19:43:54 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -18,8 +18,13 @@ require 'pry'
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
-      image_link: 
+      image_link: project.css("div.project-thumbnail a img").attribute("src").value,
+      description: project.css("p.bbcard_blurb").text, 
+      location: project.css("ul.project-meta").text,
+      percent_funded: project.css("li.first.funded strong").text.gsub("%", "").to_i
     }
-    
+  end
+  
+  projects
     
 end
